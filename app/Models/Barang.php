b886fee297e00f8690ce5441c9171c942a502703
ivankajr <ivnka24@gmail.com>
2021-03-30 08:06:46 +0700

    buat models barang

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Barang as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model; //model Eloquent
 
 class Barang extends Model
 {
-    use HasFactory;
-}
+    protected $table="barangs"; // Eloquent akan membuat model mahasiswa menyimpan record di table mahasiswas
+    public $timestamps=false;
+    protected $primaryKey = 'id_barang'; // Memanggil isi DB dengan Primarykey
+    
+    protected $fillable = [
+        'id_barang',
+        'kode_barang',
+        'nama_barang',
+        'kategori_barang',
+        'harga',
+        'qty',
+    ];
+};
